92e4bf5e09b86a8b6c80cec4ca8873d0b44c66ff
aditya aries <adiditya52@gmail.com>
2022-04-15 13:00:50 +0700

    commit

---
+++ main.py
@@ -0,0 +1,47 @@
+import requests
+from datetime import datetime
+
+USERNAME = "adityaaries"
+TOKEN = "fqdqwdefqeqdqf"
+ID = "graph1"
+TODAY = datetime.now().strftime("%Y%m%d")
+
+param = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+}
+pixel = "https://pixe.la/v1/users"
+# response = requests.post(url=pixel, json=param)
+# print(response.text)
+
+graph_endpoint = f"{pixel}/{USERNAME}/graphs"
+header = {
+    "X-USER-TOKEN": TOKEN
+}
+body = {
+    "id": ID,
+    "name": "Coding Graph",
+    "unit": "Minutes",
+    "type": "int",
+    "color": "kuro"
+}
+# response = requests.post(url=graph_endpoint, headers=header, json=body)
+# print(response.text)
+
+post_pixel = f"{graph_endpoint}/{ID}"
+config_pixel = {
+    "date": TODAY,
+    "quantity": "120",
+}
+
+# response = requests.post(url=post_pixel, json=config_pixel, headers=header)
+# print(TODAY.strftime("%Y%m%d"))
+
+update_pixel = f"{post_pixel}/{TODAY}"
+config_update_pixel = {
+    "quantity": "180",
+}
+response = requests.put(url=update_pixel, json=config_update_pixel, headers=header)
+print(response.text)
